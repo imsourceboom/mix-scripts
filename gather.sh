890a404f67b330deaa49299a43ef0355187e4aa8
imsourceboom <ecmascript.developer@gmail.com>
2022-03-19 18:01:32 +0900

    update gather scripts

--- gather.sh
+++ gather.sh
@@ -4,4 +4,4 @@ SCRIPTS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 
 cd $SCRIPTS_DIR
 tmux new -ds gather
-tmux send -t garher.0 "sleep $((RANDOM% 20000)) &&  ./transfer.sh 2>&1 | tee ~/mix-keys/gather.log" ENTER
+tmux send -t gather.0 "sleep $((RANDOM% 20000)) &&  ./transfer.sh 2>&1 | tee ~/mix-keys/gather.log" ENTER
